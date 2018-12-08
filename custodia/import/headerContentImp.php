1207e2d8f4da9d3d639ebff92abfbcccc8545821
Adam <adam.brickhill@lonetree.site>
2018-12-08 14:48:16 +0800

    Styles changed
    Confusion was huge with the flag submit button so a better name was used.

--- custodia/import/headerContentImp.php
+++ custodia/import/headerContentImp.php
@@ -1,3 +1,3 @@
 <a class="homeButton" href="index.php"><i class="fas fa-home fa-4x"></i></a>
-		<a class="submitflagButton" href="submit.php"><i class="fas fa-clipboard-list fa-4x"></i></a>
+		<a class="confirmButton" href="submit.php"><i class="fas fa-clipboard-list fa-4x"></i></a>
 		<h1 class="title"><?php echo $title; ?></h1>
