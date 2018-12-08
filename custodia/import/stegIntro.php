bb2d32ae35b7658aaac15e44aebcef1c56558872
Adam <adam.brickhill@lonetree.site>
2018-12-08 14:56:53 +0800

    Steganography Polish
    Changed description and some styling fixes

---
+++ custodia/import/stegIntro.php
@@ -0,0 +1,3 @@
+<p>
+	Steganography, latin word for of hiding messages within messages. These days the term is used to define digital objects that hide inforamtion in their metadata or other means.
+</p>
--- stegThree.php
+++ stegThree.php
@@ -10,7 +10,7 @@
 		<?php include 'custodia/import/headerContentImp.php'; ?>
 	</header>
 	<div class="fixWidth">
-		<p>Steganography, latin word for of hiding messages within messages. These days the term is used to define digital objects that hide inforamtion in their metadata or other means. Please complete all of Level 1 challenges before attempting the "red" Level 3 Steg Image. It takes a lot of time to complete.</p>
+		<?php include 'custodia/import/stegIntro.php'; ?>
 		<h2 class="stegTitle red">Steganography III</h2>
 		<img class="steg steg3" src="custodia/images/steg1.png">
 	</div>
