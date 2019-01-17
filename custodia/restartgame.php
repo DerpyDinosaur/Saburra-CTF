817c7d7526d5af333db5abe6ba87a020ecbdd45f
Adam <adam.brickhill@lonetree.site>
2019-01-18 07:11:22 +0800

    TESTING

--- custodia/restartgame.php
+++ custodia/restartgame.php
@@ -2,70 +2,53 @@
 
 $jsonUrl = "flags.json";
 
-if(isset($_GET['password'])){
-	if ($_GET['password'] == "42") {
-		try{
-			$original = [
-				"redTeam" => [
-					"flag1" => "0",
-					"flag2" => "0",
-					"flag3" => "0",
-					"flag4" => "0",
-					"flag5" => "0",
-					"flag6" => "0",
-					"flag7" => "0",
-					"flag8" => "0",
-					"flag9" => "0",
-					"flag10" => "0",
-					"flag11" => "0",
-					"flag12" => "0",
-					"flag13" => "0"
-				],
-				"blueTeam" => [
-					"flag1" => "0",
-					"flag2" => "0",
-					"flag3" => "0",
-					"flag4" => "0",
-					"flag5" => "0",
-					"flag6" => "0",
-					"flag7" => "0",
-					"flag8" => "0",
-					"flag9" => "0",
-					"flag10" => "0",
-					"flag11" => "0",
-					"flag12" => "0",
-					"flag13" => "0"
-				]
-			];
+try{
+	$original = [
+		"redTeam" => [
+			"flag1" => "0",
+			"flag2" => "0",
+			"flag3" => "0",
+			"flag4" => "0",
+			"flag5" => "0",
+			"flag6" => "0",
+			"flag7" => "0",
+			"flag8" => "0",
+			"flag9" => "0",
+			"flag10" => "0",
+			"flag11" => "0",
+			"flag12" => "0",
+			"flag13" => "0",
+			"flag14" => "0"
+		],
+		"blueTeam" => [
+			"flag1" => "0",
+			"flag2" => "0",
+			"flag3" => "0",
+			"flag4" => "0",
+			"flag5" => "0",
+			"flag6" => "0",
+			"flag7" => "0",
+			"flag8" => "0",
+			"flag9" => "0",
+			"flag10" => "0",
+			"flag11" => "0",
+			"flag12" => "0",
+			"flag13" => "0",
+			"flag14" => "0"
+		]
+	];
 
-			$jsondata = json_encode($original, JSON_PRETTY_PRINT);
+	$jsondata = json_encode($original, JSON_PRETTY_PRINT);
 
-			if(file_put_contents($jsonUrl, $jsondata)) {
-			    echo 'Data successfully saved';
-			}else{
-			    echo "error";
-			}
-		}
-		catch (Exception $e) {
-			echo "There was a problem with the PHP JSON code";
-			echo $e;
-		}
+	if(file_put_contents($jsonUrl, $jsondata)) {
+	    echo 'Data successfully saved';
+	}else{
+	    echo "error";
 	}
+}
+catch (Exception $e) {
+	echo "There was a problem with the PHP JSON code";
+	echo $e;
 }
 
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>???</title>
-</head>
-<body>
-	<form>
-		<label>Password</label>
-		<input type="text" name="password">
-		<input type="submit" name="submit">
-	</form>
-	<p>Bonus Flag: fylacterium</p>
-</body>
-</html>
