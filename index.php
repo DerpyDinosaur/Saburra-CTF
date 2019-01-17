5de7e95b5f0609876d3141b19429c0f7f8f736f1
Adam <adam.brickhill@lonetree.site>
2019-01-18 07:10:29 +0800

    Restart game feature
    Has been moved to index file

--- index.php
+++ index.php
@@ -1,4 +1,59 @@
-<?php $title = "Saburra CTF"; ?>
+<?php 
+	if (isset($_GET['restart']) && $_GET['restart'] == 'true') {
+		$jsonUrl = "custodia/flags.json";
+
+		try{
+			$original = [
+				"redTeam" => [
+					"flag1" => "0",
+					"flag2" => "0",
+					"flag3" => "0",
+					"flag4" => "0",
+					"flag5" => "0",
+					"flag6" => "0",
+					"flag7" => "0",
+					"flag8" => "0",
+					"flag9" => "0",
+					"flag10" => "0",
+					"flag11" => "0",
+					"flag12" => "0",
+					"flag13" => "0",
+					"flag14" => "0"
+				],
+				"blueTeam" => [
+					"flag1" => "0",
+					"flag2" => "0",
+					"flag3" => "0",
+					"flag4" => "0",
+					"flag5" => "0",
+					"flag6" => "0",
+					"flag7" => "0",
+					"flag8" => "0",
+					"flag9" => "0",
+					"flag10" => "0",
+					"flag11" => "0",
+					"flag12" => "0",
+					"flag13" => "0",
+					"flag14" => "0"
+				]
+			];
+
+			$jsondata = json_encode($original, JSON_PRETTY_PRINT);
+
+			if(file_put_contents($jsonUrl, $jsondata)) {
+			    echo 'Data successfully saved';
+			}else{
+			    echo "error";
+			}
+		}
+		catch (Exception $e) {
+			echo "There was a problem with the PHP JSON code";
+			echo $e;
+		}
+	}
+
+	$title = "Saburra CTF"; 
+?>
 <!DOCTYPE html>
 <html class="loadingPage">
 <head>
