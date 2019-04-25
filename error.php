e741a85f0065523348da12ac1a1a754d2d9faaec
Adam <adam.brickhill@lonetree.site>
2019-04-25 22:13:02 +0800

    Added error page

---
+++ error.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>?????</title>
+	<style type="text/css">
+		*{
+			font-family: "Trebuchet MS", Helvetica, sans-serif;
+			font-size: 1.33em;
+			text-align: center;
+			padding: 0;
+			margin: 0;
+		}
+
+		img{
+			width: 20%;
+			padding: 2em 0;
+		}
+	</style>
+</head>
+<body>
+	<img src="pika.png">
+	<h1>You broke the website silly buns</h1>
+</body>
+</html>
